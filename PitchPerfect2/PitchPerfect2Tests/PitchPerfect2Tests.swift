d583ecf979ebe656e1a89e86752bf18b556e68e8
x07sachin <x07sachin@iimahd.ernet.in>
2015-04-07 00:20:50 +0530

    Committed by Udacity

---
+++ PitchPerfect2/PitchPerfect2Tests/PitchPerfect2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PitchPerfect2Tests.swift
+//  PitchPerfect2Tests
+//
+//  Created by Sachin Sapte on 04/04/15.
+//  Copyright (c) 2015 Sach Kas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PitchPerfect2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
