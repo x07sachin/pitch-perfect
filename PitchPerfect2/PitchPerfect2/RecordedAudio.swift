788b7cc281bf1e45e6f1135a9e37056dfa8bdf9d
x07sachin <x07sachin@iimahd.ernet.in>
2015-04-07 00:20:48 +0530

    Committed by Udacity

---
+++ PitchPerfect2/PitchPerfect2/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect2
+//
+//  Created by Sachin Sapte on 05/04/15.
+//  Copyright (c) 2015 Sach Kas. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
